6a8dabdfd066f23a4c994e14f5e58b42f8fadb0f
MrDoritos <ianmoore322@gmail.com>
2019-03-10 03:19:52 +0100

    ARM cross-compilation

--- compile.sh
+++ compile.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-gpp_args='-std=c++11 -Idata/ -Ihost/ -ggdb'
-gpp_sources=' '
-g++ $gpp_args host/host.cpp -o a.out
-
+gpp_args='-std=c++11 -Idata/ -Ihost/'
+gpp_sources='./host/axisControl.cpp ./host/utils.cpp'
+g++ $gpp_args -ggdb $gpp_sources host/host.cpp -o a.out
+g++ -std=c++11 -Idata/ -Ihost/ -O2 -s $gpp_sources host/host.cpp -o optimized_x64.out
+arm-linux-gnueabihf-g++ $gpp_args -ggdb $gpp_sources host/host.cpp -o arm.out
+arm-linux-gnueabihf-g++ $gpp_args -O2 -s $gpp_sources host/host.cpp -o optimized_arm.out
