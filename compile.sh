e60e04a78381e29481d6bc34b38f45188f6b5fa2
MrDoritos <ianmoore322@gmail.com>
2019-03-10 14:52:18 +0100

    Added GCC define

--- compile.sh
+++ compile.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-gpp_args='-std=c++11 -Idata/ -Ihost/'
+gpp_args='-std=c++11 -Idata/ -Ihost/ -DGCC'
 gpp_sources='./host/axisControl.cpp ./host/utils.cpp'
 g++ $gpp_args -ggdb $gpp_sources host/host.cpp -o a.out
 g++ -std=c++11 -Idata/ -Ihost/ -O2 -s $gpp_sources host/host.cpp -o optimized_x64.out
